3c5efb589faccd2160f231ff62f28bc9d53ab9d4
Wolverlone <gamesmews@hotmail.com>
2024-02-06 01:44:04 +0700

    Workshop 5 Step 2&3 - Health Display & Dealing Damage

--- WorkshopMultiplayer/Assets/Scripts/Core/Player/ProjectileLauncher.cs
+++ WorkshopMultiplayer/Assets/Scripts/Core/Player/ProjectileLauncher.cs
@@ -71,6 +71,11 @@ public class ProjectileLauncher : NetworkBehaviour
 
         Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());
 
+        if(projectileInstance.TryGetComponent<DealDamageOnContact>(out DealDamageOnContact dealDamage))
+        {
+            dealDamage.SetOwner(OwnerClientId);
+        }
+
         if (projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
         {
             rb.velocity = rb.transform.up * projectileSpeed;
