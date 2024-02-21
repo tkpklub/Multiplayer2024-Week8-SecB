d58331d69d78860665e06b24576c981c7b5ff1fd
Wolverlone <gamesmews@hotmail.com>
2024-02-21 23:18:53 +0700

    Complete 7-01

--- WorkshopMultiplayer/Assets/Scripts/Networking/Client/AuthenticationWrapper.cs
+++ WorkshopMultiplayer/Assets/Scripts/Networking/Client/AuthenticationWrapper.cs
@@ -52,17 +52,17 @@ public static class AuthenticationWrapper
                     break;
                 }
             }
-            catch (AuthenticationException ex)
+            catch (AuthenticationException authException)
             {
-                Debug.LogError(ex);
+                Debug.LogError(authException);
                 AuthState = AuthState.Error;
             }
-            catch(RequestFailedException exeption)
+            catch (RequestFailedException requestException)
             {
-                Debug.LogError(exeption);
+                Debug.LogError(requestException);
                 AuthState = AuthState.Error;
             }
-            
+
             retries++;
             await Task.Delay(1000);
         }
