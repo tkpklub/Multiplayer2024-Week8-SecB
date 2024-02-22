72ee194d01e9cafced088018a6609551cefa7899
Wolverlone <gamesmews@hotmail.com>
2024-02-22 07:35:36 +0700

    Complete 7-05

--- WorkshopMultiplayer/Assets/Scripts/Networking/Host/HostGameManager.cs
+++ WorkshopMultiplayer/Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -5,15 +5,19 @@ using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class HostGameManager
 {
    private Allocation allocation;
    private string joinCode;
+   private string lobbyId;
 
    private const int MaxConnections = 20;
    private const string GameSceneName = "Game";
@@ -45,8 +49,43 @@ public class HostGameManager
         RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
         transport.SetRelayServerData(relayServerData);
 
+        try
+        {
+            CreateLobbyOptions lobbyOptions = new CreateLobbyOptions();
+            lobbyOptions.IsPrivate = false;
+            lobbyOptions.Data = new Dictionary<string, DataObject>()
+            {
+                {
+                    "JoinCode", new DataObject(
+                        visibility: DataObject.VisibilityOptions.Member,
+                    value: joinCode
+                    )
+                }
+            };
+            Lobby lobby = await Lobbies.Instance.CreateLobbyAsync(
+                "My Lobby", MaxConnections, lobbyOptions);
+            lobbyId = lobby.Id;
+
+            HostSingleton.Instance.StartCoroutine(HeartbeatLobby(15));
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+            return;
+        }
+
         NetworkManager.Singleton.StartHost();
 
         NetworkManager.Singleton.SceneManager.LoadScene(GameSceneName, LoadSceneMode.Single);
+    }
+
+    private IEnumerator HeartbeatLobby(float waitTimeSeconds)
+    {
+        WaitForSecondsRealtime delay = new WaitForSecondsRealtime(waitTimeSeconds);
+        while(true)
+        {
+            Lobbies.Instance.SendHeartbeatPingAsync(lobbyId);
+            yield return delay;
+        }
     }
 }
